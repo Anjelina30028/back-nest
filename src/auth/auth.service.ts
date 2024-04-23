d13e79870e180496718df3a4f633a21ec85d38e6
Anjelina30028 <nevazno30028@gmail.com>
2024-04-23 15:16:44 +0700

    first commit
    first commit

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,84 @@
+import {
+	BadRequestException,
+	Injectable,
+	UnauthorizedException,
+} from '@nestjs/common'
+import { ModelType } from '@typegoose/typegoose/lib/types'
+import { InjectModel } from 'nestjs-typegoose'
+import { UserModel } from 'src/user/user.model'
+import { AuthDto } from './dto/auth.dto'
+import { hash, genSalt, compare } from 'bcryptjs'
+import passport from 'passport'
+import { JwtService } from '@nestjs/jwt'
+
+@Injectable()
+export class AuthService {
+	constructor(
+		@InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>,
+		private readonly jwtService: JwtService
+	) {}
+
+	async login(dto: AuthDto) {
+		const user = await this.validateUser(dto)
+		const tokens = await this.issueTokenPair(String(user._id))
+
+		return {
+			user: this.returnUserFields(user),
+			...tokens,
+		}
+	}
+
+	async register(dto: AuthDto) {
+		const oldUser = await this.UserModel.findOne({ email: dto.email })
+		if (oldUser)
+			throw new BadRequestException('Пользователь с таким email уже существует')
+
+		const salt = await genSalt(10)
+
+		const newUser = new this.UserModel({
+			email: dto.email,
+			password: await hash(dto.password, salt),
+		})
+
+		const tokens = await this.issueTokenPair(String(newUser._id))
+
+		return {
+			user: this.returnUserFields(newUser),
+			...tokens,
+		}
+	}
+
+	async validateUser(dto: AuthDto): Promise<UserModel> {
+		const user = await this.UserModel.findOne({ email: dto.email })
+		if (!user)
+			throw new UnauthorizedException('Пользователь с таким email не найден')
+
+		const isValidPassword = await compare(dto.password, user.password)
+		if (!isValidPassword)
+			throw new UnauthorizedException('Вы ввели неверный пароль')
+
+		return user
+	}
+
+	async issueTokenPair(userId: string) {
+		const data = { _id: userId }
+
+		const refreshToken = await this.jwtService.signAsync(data, {
+			expiresIn: '15d',
+		})
+
+		const accessToken = await this.jwtService.signAsync(data, {
+			expiresIn: '1h',
+		})
+
+		return { refreshToken, accessToken }
+	}
+
+	returnUserFields(user: UserModel) {
+		return {
+			_id: user._id,
+			email: user.email,
+			isAdmin: user.isAdmin,
+		}
+	}
+}
---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,27 @@
+import { PassportStrategy } from '@nestjs/passport'
+import { ExtractJwt, Strategy } from 'passport-jwt'
+import { ConfigService } from '@nestjs/config'
+import { InjectModel } from 'nestjs-typegoose'
+import { UserModel } from 'src/user/user.model'
+import { ModelType } from '@typegoose/typegoose/lib/types'
+import { Injectable } from '@nestjs/common'
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+	constructor(
+		private readonly configService: ConfigService,
+		@InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>
+	) {
+		super({
+			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+			ignoreExpiration: false,
+			secretOrKey: configService.get('JWT_SECRET'),
+		})
+	}
+
+	async validate({ _id }: Pick<UserModel, '_id'>) {
+		return await this.UserModel.findById(_id).exec()
+        // const user = await this.UserModel.findById(_id)
+		// return user
+	}
+}
